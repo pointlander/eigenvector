298b67c0c7b22a57c534a8fa763fc66bca97c661
Andrew Snodgrass <pointlander@gmail.com>
2025-11-17 08:48:40 -0600

    Added inference

--- main.go
+++ main.go
@@ -730,6 +730,51 @@ func AAMode() {
 	if err != nil {
 		panic(err)
 	}
+
+	tests := make([]tf64.Set, len(aa[0].Test))
+	for s := range tests {
+		factor(&tests[s], aa[0].Test[s].Input)
+	}
+	for s := range tests {
+		l1 := tf64.Everett(tf64.Add(tf64.Mul(ff.Get("l1"), tests[s].Get("i")), ff.Get("b1")))
+		l2 := tf64.Add(tf64.Mul(ff.Get("l2"), l1), ff.Get("b2"))
+		l2(func(a *tf64.V) bool {
+			for i := range maxY {
+				for ii := range maxX {
+					max, symbol := 0.0, 0
+					for iii := range Symbols {
+						if x := a.X[(i*maxX+ii)*Symbols+iii]; x > max {
+							max, symbol = x, iii
+						}
+					}
+					if symbol == 10 {
+						fmt.Printf("U")
+					} else {
+						fmt.Printf("%c", symbol+'0')
+					}
+				}
+				fmt.Println()
+			}
+			return true
+		})
+		fmt.Println()
+		output := aa[0].Test[s].Output
+		for y := range output {
+			for _, value := range output[y] {
+				fmt.Printf("%c", '0'+value)
+			}
+			for range maxX - len(output[y]) {
+				fmt.Printf("U")
+			}
+			fmt.Println()
+		}
+		for range maxY - len(output) {
+			for range maxX {
+				fmt.Printf("U")
+			}
+			fmt.Println()
+		}
+	}
 }
 
 // MPRMode is the markov page rank mode
